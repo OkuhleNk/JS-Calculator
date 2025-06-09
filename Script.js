ba7557e5941db923ed7271d9e9d87dce0f14ee08
OkuhleNk <lindokuhleh095@gmail.com>
2025-06-09 16:48:53 +0000

    LESSON3:Remove 0 in the display

--- Script.js
+++ Script.js
@@ -7,6 +7,32 @@ let justcalculated =false;
 function appendTodisplay(value) {
     console.log('button pressed:',value);
     alert('You pressed:'+value);
+    
+    let currentValue=display.value;
+
+    if(justCalculated && !isNaN(value)){
+        display.value=value;
+    justcalculated=false;
+    return;
+    }
+    //If current display show 0 and user enters a number,we wanna replace the 0
+    if(current==="0"&& !isNaN(value)){
+       display.value= value; 
+    } 
+
+    //If the current display shows 0 and the user enters a decimal ,keep the 0
+    else if(currentValue==='0' && value==='.'){
+        display.value=currentvalue + value
+     }
+     else{
+        display.value=currentValue + value;
+     }
+
+     //Reset the justCalculated flag when user starts typing
+     justcalculated=false;
+
+     console.log('Diplay updated to: ',display.value);
+   
 }
 function clearDisplay() {
     console.log('Clear button pressed.')
@@ -14,6 +40,15 @@ function clearDisplay() {
 }
 function deletelasat() {
     console.log('Backspace  button pressed.')
+    let currentValue= display.value
+
+    //If theres only one characteror its 0,reset to 0
+    if(currentvalue.length<=1|| currentValue==='0'){
+        display.value='0';
+    }else{
+        display.value=currentValue.slice(0,-1);
+    }
+
     alert(Backspace button was clicked);
 }
 function CALCULATE() {
